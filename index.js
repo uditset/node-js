d11d9dbd3f21962f2b0a5427d5762ed0c6f205a6
udit.sethi <udit.sethi@concentrix.com>
2024-03-30 17:20:40 +0530

    Done

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
-// import { students } from "./script/constants";
-const appConstants = require('./script/constants');
-const http = require('http');
-const {writeFileSync} = require('fs');
-const color = require('colors');
-// const chalk = require('chalk').default;
-http.createServer((request,response)=>{
-    response.write("This node server is running at port 4500");
-    response.end();
-}).listen(4500);
+const fs = require("fs");
+const path = require("path");
+fs.writeFileSync("random11.txt","Hey bro sup");
+const dirName = path.join(__dirname,"crud files");
+//  fs.writeFileSync(dirName + "/random11.txt","Hey bro sup in crud files folder.");
+//  for(i=0;i<3;i++)
+//  {
+//      fs.writeFileSync(dirName + `/random${i}.txt`,`Hey bro sup ${i} in crud files folder.`);
+//  }
 
-const a = 10; 
-const b = 20;
-console.log(a+b);
-for(i=1;i<=10;i++)
-{
-    console.log(i);
-}
-const students = appConstants?.students;
-const dummyVariable = appConstants?.dummyVariable?.t;
-const variable = appConstants?.variable;
-const constant = appConstants?.constant;
-console.log(color.bgMagenta(students));
-console.log(dummyVariable);
-console.log(variable===20);
-console.log(constant===20);
-students?.map((item)=>console.log(item?.name));
-console.log(students?.filter(item=>item.name==="Udit"));
-console.log(variable?.length)
-variable?.length && console.log(variable?.filter(item => item.value==20));
-writeFileSync("onlyFunction.text","Dobara run kr diya This is my first creating and writting in file test to learn what is non global module.");
-console.log(__dirname);
-console.log(__filename);
+fs.readFile(dirName+ `/random1.txt`,(error,item)=>{
+    console.log(item);
+})
+
+fs.appendFile(dirName+ `/random0.txt`," so this is after updating bro.",err => console.log(!err ? "file is updated successfully" : "there is some error in appending in the file."));
+fs.rename(dirName+ `/random0.txt`,dirName+ `/random00.txt`,err => console.log(!err ? "file renamed successfully" : "there is some error in renaming the file."));

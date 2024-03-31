4e1b97f9ba617ab72956c5df91551350663db5c2
udit.sethi <udit.sethi@concentrix.com>
2024-03-31 16:15:42 +0530

    dealing with page loading and express api

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const http = require('http');
-const constant = require("./script/constants");
-http.createServer((req,resp)=>{
-    resp.writeHead(200,{"content-type":"application/json"});
-    resp.write(JSON.stringify(constant));
-    resp.end();
-}).listen(50000);
+const express = require('express');
+const app = express();
+const constObject = require("./script/constants");
+const path = require("path")
+const port = 10000;
+const pagesPath = path.join(__dirname,"pages");
 
-const promiseObj = new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        reject("error hai");
-    },5000);
+app.listen(port,()=> console.log(`App is running on the port ${port}`));
+
+app.use(express.static(pagesPath));
+
+
+app.get("/",(req,response)=>{
+    response.send(`
+        <div style="background: orange;">
+            <h1>This is the home page</h1>
+            <label for="name"> Name </label>
+            <input type="text" placeholder="Enter your name" id="name" name="name" style="border: 1px solid blue">
+            <a href="/aboutus">About us</a>
+            <a href="/">Home</a>
+        </div>
+    `);
 })
-console.log(y);
-let y;
-promiseObj.then((item)=>console.log(item)).catch(error => console.log(error));
-console.log("Hey baby");
+
+app.get("/aboutUs",(req,response)=>{
+    response.send(`
+    <div>
+        This is the About us page
+        <a href="/">Home</a>
+    </div>
+    `);
+})
+
+app.post("/aboutUs",(req,resp)=>{
+    console.log("recieved request:       ");
+    console.log(req);
+    resp.status(200);
+    resp.json(constObject);
+})
+

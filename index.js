f8f9143b070e05ef9fe069c67f3f0d32b2eefe11
udit.sethi <udit.sethi@concentrix.com>
2024-03-31 14:34:13 +0530

    async revision

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
-// import { students } from "./script/constants";
-const appConstants = require('./script/constants');
 const http = require('http');
-const {writeFileSync} = require('fs');
-const color = require('colors');
-// const chalk = require('chalk').default;
-http.createServer((request,response)=>{
-    response.write("This node server is running at port 4500");
-    response.end();
-}).listen(4500);
+const constant = require("./script/constants");
+http.createServer((req,resp)=>{
+    resp.writeHead(200,{"content-type":"application/json"});
+    resp.write(JSON.stringify(constant));
+    resp.end();
+}).listen(50000);
 
-const a = 10; 
-const b = 20;
-console.log(a+b);
-for(i=1;i<=10;i++)
-{
-    console.log(i);
-}
-const students = appConstants?.students;
-const dummyVariable = appConstants?.dummyVariable?.t;
-const variable = appConstants?.variable;
-const constant = appConstants?.constant;
-console.log(color.bgMagenta(students));
-console.log(dummyVariable);
-console.log(variable===20);
-console.log(constant===20);
-students?.map((item)=>console.log(item?.name));
-console.log(students?.filter(item=>item.name==="Udit"));
-console.log(variable?.length)
-variable?.length && console.log(variable?.filter(item => item.value==20));
-writeFileSync("onlyFunction.text","Dobara run kr diya This is my first creating and writting in file test to learn what is non global module.");
-console.log(__dirname);
-console.log(__filename);
+const promiseObj = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        reject("error hai");
+    },5000);
+})
+console.log(y);
+let y;
+promiseObj.then((item)=>console.log(item)).catch(error => console.log(error));
+console.log("Hey baby");
